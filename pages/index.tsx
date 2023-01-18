6f051eb2e1c2bcaf0bc07c05f2c45cc35b51f251
ragingrahul <raj17sarma@gmail.com>
2023-01-18 12:46:24 +0530

    Avatar Ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 
 import { Inter, } from '@next/font/google'
-import {MdOutlineAdd,MdOutlineDone} from 'react-icons/md'
+import { MdOutlineAdd, MdOutlineDone } from 'react-icons/md'
 import { AnchorProvider, Wallet } from '@project-serum/anchor'
 import { SocialProtocol } from '@spling/social-protocol'
 import { AnchorWallet, useAnchorWallet, useWallet } from '@solana/wallet-adapter-react'
 import { clusterApiUrl, ConfirmOptions, Connection, Keypair, PublicKey } from '@solana/web3.js'
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import { ProtocolOptions, User } from '@spling/social-protocol/dist/types'
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
 import { WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
@@ -34,9 +34,13 @@ export default function Home() {
   const [walletAddress, setWalletAddress] = useState()
   const [userInfo, setUserInfo] = useState<User | null>()
   const [loading, setLoading] = useState<boolean>(false)
-  const [cardio,setCardio]=useState<boolean>(false)
-  const [weight,setWeight]=useState<boolean>(false)
-  const [yoga,setYoga]=useState<boolean>(false)
+  const [cardio, setCardio] = useState<boolean>(false)
+  const [weight, setWeight] = useState<boolean>(false)
+  const [yoga, setYoga] = useState<boolean>(false)
+  const [userName,setUserName]=useState<string>('')
+  const [avatar,setAvatar] = useState<File>()
+
+  const avatarRef=useRef<HTMLInputElement>(null)
 
   const solWal = useWallet()
 
@@ -111,43 +115,90 @@ export default function Home() {
       </>
     )
   }
- 
+
   const CreateUserUI = () => {
     return (
       <>
         <h2 className='text-3xl mt-[40px] font-normal'>It seems you are a newbie</h2>
-        <input type='text' placeholder="Name" className='rounded-2xl my-[20px] p-2 w-[300px] border-4 border-[#A0D8EF]' />
-        <div className='flex flex-row text-left mb-[10px]'>
-          <button className={`${cardio?`bg-[#A9EFA0]`:`bg-[#A0D8EF]`} rounded-full mx-[10px] px-10 py-1 font-normal hover:cursor-default`}>
-            Cardio
-          </button>
-          {!cardio?
-            <MdOutlineAdd size={40} color={"white"}className='bg-[#A0D8EF] rounded-full' onClick={()=>setCardio(!cardio)}/>
-            :
-            <MdOutlineDone size={40} color={"white"}className='bg-[#A9EFA0] rounded-full' onClick={()=>setCardio(!cardio)}/>
-          }
-          
+        <input 
+          value={userName} 
+          type='text' 
+          placeholder="Name" 
+          className='rounded-2xl my-[20px] p-2 w-[300px] border-4 border-[#A0D8EF] focus:outline-none text-[#565656]' 
+          onChange={(e)=>{
+            if(e.target)
+            setUserName(e.target.value)
+          }}
+        />
+
+        <div className='flex flex-row justify-around'>
+          <div 
+            onClick={()=>{
+              avatarRef?.current?.click()
+            }}
+            className='h-[140px] w-[140px] rounded-full hover:cursor-pointer'
+          >
+            {avatar ? (
+              <img 
+                onClick={()=>{
+                  avatarRef?.current?.click()
+                }}
+                src={URL.createObjectURL(avatar)}
+                alt='avatar'
+                className='rounded-full h-[140px] w-[140px] border-4 border-[#A0D8EF]'
+              />
+            ):(
+              <img src='/ProfilePic.png' alt='ProfilePic'/>
+            )}
+          </div>
+          <input 
+            type="file"
+            className="hidden"
+            ref={avatarRef}
+            onChange={(e)=>{
+              if(!e.target.files)
+                return
+              setAvatar(e.target.files[0])
+              console.log(e.target.files[0].type)
+            }}
+          />
+         
+          <div className='flex flex-col justify-start'>
+            <div className='flex flex-row text-left mb-[10px]'>
+              <button className={`${cardio ? `bg-[#A9EFA0]` : `bg-[#A0D8EF]`} rounded-full mx-[10px] px-10 py-1 font-normal hover:cursor-default`}>
+                Cardio
+              </button>
+              {!cardio ?
+                <MdOutlineAdd size={40} color={"white"} className='bg-[#A0D8EF] rounded-full hover:cursor-pointer' onClick={() => setCardio(!cardio)} />
+                :
+                <MdOutlineDone size={40} color={"white"} className='bg-[#A9EFA0] rounded-full hover:cursor-pointer' onClick={() => setCardio(!cardio)} />
+              }
+
+            </div>
+            <div className='flex flex-row text-left mb-[10px]'>
+              <button className={`${weight ? `bg-[#A9EFA0]` : `bg-[#A0D8EF]`} rounded-full mx-[10px] px-10 py-1 font-normal hover:cursor-default`}>
+                Weight Training
+              </button>
+              {!weight ?
+                <MdOutlineAdd size={40} color={"white"} className='bg-[#A0D8EF] rounded-full hover:cursor-pointer' onClick={() => setWeight(!weight)} />
+                :
+                <MdOutlineDone size={40} color={"white"} className='bg-[#A9EFA0] rounded-full hover:cursor-pointer' onClick={() => setWeight(!weight)} />
+              }
+            </div>
+            <div className='flex flex-row text-left mb-[10px]'>
+              <button className={`${yoga ? `bg-[#A9EFA0]` : `bg-[#A0D8EF]`} rounded-full mx-[10px] px-10 py-1 font-normal hover:cursor-default`}>
+                Yoga
+              </button>
+              {!yoga ?
+                <MdOutlineAdd size={40} color={"white"} className='bg-[#A0D8EF] rounded-full hover:cursor-pointer' onClick={() => setYoga(!yoga)} />
+                :
+                <MdOutlineDone size={40} color={"white"} className='bg-[#A9EFA0] rounded-full hover:cursor-pointer' onClick={() => setYoga(!yoga)} />
+              }
+            </div>
+          </div>
         </div>
-        <div className='flex flex-row text-left mb-[10px]'>
-          <button className={`${weight?`bg-[#A9EFA0]`:`bg-[#A0D8EF]`} rounded-full mx-[10px] px-10 py-1 font-normal hover:cursor-default`}>
-            Weight Training
-          </button>
-          {!weight?
-            <MdOutlineAdd size={40} color={"white"}className='bg-[#A0D8EF] rounded-full' onClick={()=>setWeight(!weight)}/>
-            :
-            <MdOutlineDone size={40} color={"white"}className='bg-[#A9EFA0] rounded-full' onClick={()=>setWeight(!weight)}/>
-          }
-        </div>
-        <div className='flex flex-row text-left mb-[10px]'>
-          <button className={`${yoga?`bg-[#A9EFA0]`:`bg-[#A0D8EF]`} rounded-full mx-[10px] px-10 py-1 font-normal hover:cursor-default`}>
-            Yoga
-          </button>
-          {!yoga?
-            <MdOutlineAdd size={40} color={"white"}className='bg-[#A0D8EF] rounded-full' onClick={()=>setYoga(!yoga)}/>
-            :
-            <MdOutlineDone size={40} color={"white"}className='bg-[#A9EFA0] rounded-full' onClick={()=>setYoga(!yoga)}/>
-          }
-        </div>
+
+
         <button className='bg-[#A0D8EF] rounded-full mt-[30px] px-14 py-3 font-medium' onClick={connectWallet}>
           Become a member
         </button>
